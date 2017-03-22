a56705bf9cdb72c3ae994dcd2cb74a224d442d9d
meltem kilic <melltemkilic@gmail.com>
2017-03-22 17:21:33 +0000

    hover state is set to the tweet section

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,30 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+ var tweetData = {
+   "user": {
+     "name": "Newton",
+     "avatars": {
+       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+     },
+     "handle": "@SirIsaac"
+   },
+   "content": {
+     "text": "If I have seen further it is by standing on the shoulders of giants"
+   },
+   "created_at": 1461116232227
+ };
 
+function createTweetElement(obj) {
+  var $tweet = $("<article>").addClass("tweet");
+  $(".container").append($tweet);
+  return $tweet;
+}
+
+var $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet);
+$('#tweets-container').append($tweet);
