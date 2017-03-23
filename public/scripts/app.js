137afb3a918bd47dca80e4630cb3643006d534c3
meltem kilic <melltemkilic@gmail.com>
2017-03-23 00:46:53 +0000

    Fetching tweets with Ajax, Form Validation steps completed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,60 +3,14 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
- var data = [
-   {
-     "user": {
-       "name": "Newton",
-       "avatars": {
-         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-       },
-       "handle": "@SirIsaac"
-     },
-     "content": {
-       "text": "If I have seen further it is by standing on the shoulders of giants"
-     },
-     "created_at": 1461116232227
-   },
-   {
-     "user": {
-       "name": "Descartes",
-       "avatars": {
-         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-       },
-       "handle": "@rd" },
-     "content": {
-       "text": "Je pense , donc je suis"
-     },
-     "created_at": 1461113959088
-   },
-   {
-     "user": {
-       "name": "Johann von Goethe",
-       "avatars": {
-         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-       },
-       "handle": "@johann49"
-     },
-     "content": {
-       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-     },
-     "created_at": 1461113796368
-   }
- ];
 
 function createTweetElement(obj) {
-  const ele = `
-    <article class="tweet">
-      <header><img src="${ obj.user.avatars.small }"</header>
-      <h1>${ obj.user.name }</h1>
-    </article>
-  `
+//   const ele = `
+//     <article class="tweet">
+//       <header><img src="${ obj.user.avatars.small }"</header>
+//       <h1>${ obj.user.name }</h1>
+//     </article>
+//   `
 
   var $tweet = $("<article>").addClass("tweet")
   .append($("<header>").append("<img src='" + obj.user.avatars.small + "'>")
@@ -68,28 +22,45 @@ function createTweetElement(obj) {
 }
 
 function renderTweets(tweets) {
-  var $container = $('.container');
+  var $container = $('.tweets-container');
+  // $('.tweet').remove();
   for (i in tweets) {
     var $elem = createTweetElement(tweets[i]);
-    $container.append($elem);
+    $container.prepend($elem);
     console.log($elem);
   }
 }
 
 $(document).ready(function() {
-  renderTweets(data);
-
 /* attach a submit handler to the form */
   $("#compose").on('submit', function (event) {
-    $.ajax({
+    event.preventDefault();
+    if($("textarea").val() === "" || $(this).val() === null) {
+      return alert("Tweet content is not present.");
+    } else if ($("textarea").val().length > 140) {
+      return alert("Tweet content exceeds the maximum limit.");
+    } else {
+      /* stop form from submitting normally */
+      $.ajax({
         url: '/tweets',
         method: 'POST',
         data: $(this).serialize(),
-        success: function (data) {
-          console.log(data);
+        success: function () {
+          loadTweets();
         }
       });
-      /* stop form from submitting normally */
-      event.preventDefault();
+    }
+  });
+
+  function loadTweets() {
+    console.log('Tweet sent, performing ajax call...');
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: renderTweets
     });
+  };
+
+  loadTweets();
+
 });
