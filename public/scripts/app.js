c8971d9cd34b55425e7e0010545373dda9876018
meltem kilic <melltemkilic@gmail.com>
2017-03-24 12:26:59 -0400

    text area get blank after a tweet is posted

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,23 +1,12 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 
 function createTweetElement(obj) {
-//   const ele = `
-//     <article class="tweet">
-//       <header><img src="${ obj.user.avatars.small }"</header>
-//       <h1>${ obj.user.name }</h1>
-//     </article>
-//   `
 
   var $tweet = $("<article>").addClass("tweet")
   .append($("<header>").append("<img src='" + obj.user.avatars.small + "'>")
   .append($("<h1>").text(obj.user.name))
   .append($("<h2>").text(obj.user.handle)))  //appending everyting under header
   .append($("<div>").addClass("tweet-body").append($("<p>").text(obj.content.text))) //appending everyting under div-tweet-body
-  .append($("<footer>").append($("<p>").text(getTheCurrentTime(obj.created_at))).append( $("<span>").append("<i class='fa fa-flag' aria-hidden='true'>").append("<i class='fa fa-retweet' aria-hidden='true'>").append("<i class='fa fa-heart' aria-hidden='true'>")));
+  .append($("<footer>").append($("<p>").text(getTheCurrentTime(obj.created_at))).append($("<span>").append("<i class='fa fa-flag' aria-hidden='true'>").append("<i class='fa fa-retweet' aria-hidden='true'>").append("<i class='fa fa-heart' aria-hidden='true'>")));
   return $tweet;
 }
 
@@ -63,6 +52,7 @@ $(document).ready(function() {
         data: $(this).serialize(),
         success: function () {
           loadTweets();
+          $("textarea").val("");
         }
       });
     }
